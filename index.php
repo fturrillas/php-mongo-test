0d7076e7ea01ed5c464502c25d78dfbf53a094b2
OSE Admin <oseadmin@vas.entelpcs.cl>
2020-04-28 11:36:05 -0400

    2

--- index.php
+++ index.php
@@ -7,11 +7,10 @@ $mongo_user=getenv( "MONGODB_USER" );
 $mongo_password= getenv("MONGODB_PASSWORD");
 $mongo_database=getenv("MONGODB_DATABASE" );
 
-/*
-echo "server $mongo_server\n";
-echo "user $mongo_user\n";
-echo "password $mongo_password\n";
-echo "database $mongo_database\n";
+echo "server   =  $mongo_server\n";
+echo "user     =  $mongo_user\n";
+echo "password = $mongo_password\n";
+echo "database = $mongo_database\n";
 
 
 require 'MongoDB/autoload.php';
@@ -20,7 +19,6 @@ require 'MongoDB/autoload.php';
 $cliente = new MongoDB\Client("mongodb://localhost:27017");
 
 phpinfo();
-*/
 
 
 
